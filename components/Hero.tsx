cd0a9849a8dac05cd329c032c653f2a5459d927c
Arturo Campos-Rodríguez <ascampos@alum.mit.edu>
2022-06-22 12:02:07 -0400

    border

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -70,7 +70,7 @@ function classNames(...classes: string[]) {
 export default function Example() {
   return (
     <div className='px-4 pt-16 pb-20 bg-gray-50 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8'>
-      <div className='relative max-w-lg mx-auto divide-y-2 divide-gray-200 lg:max-w-7xl'>
+      <div className='relative max-w-lg mx-auto lg:max-w-7xl'>
         <div>
           <h2 className='text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl'>
             GRIT
