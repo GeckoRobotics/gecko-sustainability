6aa780b70bb3cd1426bd0c86ad0b4f4ea8f8dbce
Arturo Campos-Rodríguez <ascampos@alum.mit.edu>
2022-06-23 12:36:23 -0400

    mroe

--- pages/recycling.tsx
+++ pages/recycling.tsx
@@ -1,10 +1,41 @@
 import { Layout } from '.'
+// @ts-ignore
+import file from '../assets/no-plastic-bags.pdf'
+// @ts-ignore
+
+import file2 from '../assets/recycling-right.pdf'
+// @ts-ignore
+
+import file3 from '../assets/recycling-toolkit.pdf'
 
 const Recycling = () => {
   return (
     <Layout>
       <div className='relative flex items-center justify-center h-screen overflow-hidden bg-gray-50'>
-        Recycling
+        <div className='relative flex items-center justify-center h-screen overflow-hidden bg-gray-50'>
+          {/* Global */}
+          <iframe
+            style={{ width: '563px', height: '666px' }}
+            src={file}
+            //@ts-ignore
+            type='application/pdf'
+            title='title'
+          />
+          <iframe
+            style={{ width: '563px', height: '666px' }}
+            src={file2}
+            //@ts-ignore
+            type='application/pdf'
+            title='title'
+          />
+          <iframe
+            style={{ width: '563px', height: '666px' }}
+            src={file3}
+            //@ts-ignore
+            type='application/pdf'
+            title='title'
+          />
+        </div>
       </div>
     </Layout>
   )
